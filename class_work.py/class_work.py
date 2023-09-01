a0a8612ede8cda37e97e91b36446c2ee3a689074
MaiyegunM <muizmaiyegun@gmail.com>
2023-09-01 11:12:23 +0100

    zed01

---
+++ class_work.py/class_work.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print  ("10 lines of a file at the top and bottom is- head and tail respectively")
+print  ("search through a file - grep")
+print = ("to check in a file - cat")
+print  ("to create a file - touch")
+print  ("move - mv / copy - cp")
+print  ("to check in a file - cat")
+
+
+
+
---
+++ keygendisp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import random
+
+print("this is my first python program and script")
+
+
+
+print("*********************************************")
+print("*********************************************")
+print("***       ***         ******    *****      **")
+print("*** **  ** **                          ******")
+print("***   **   **         ******             ****")
+print("***        **         ******               **")
+print("*********************************************")
+print("*********************************************")
+
+user_number= random.randint(1,40)
+user_number=[]
+
+print("this is my first python program and script")
